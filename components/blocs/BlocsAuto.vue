9003cefe5d855ab6def4b5ad8b94f6747e24cc81
Vincent Dilly <vdilly.prod@gmail.com>
2022-08-03 12:52:00 +0200

    bloc content SBS

--- components/blocs/BlocsAuto.vue
+++ components/blocs/BlocsAuto.vue
@@ -2,16 +2,17 @@
 .blocs
   .bloc(v-for="(bloc, index) in blocs")
     Container.bloc-name(v-if="displayName")
-      h2.h2(v-html="index + ' ' + getBlocName(bloc) + '.vue'")
+      h2.h2(v-html="index + ' - ' + getBlocName(bloc) + '.vue'")
     component(:is="getBlocName(bloc)", :bloc="bloc")
 </template>
 <script>
 import BannerFull from "@/components/blocs/Banners/BannerFull.vue";
 import BannerStack from "@/components/blocs/Banners/BannerStack.vue";
 import BlogPosts from "@/components/blocs/BlogPosts.vue";
+import ContentSBS from "@/components/blocs/ContentSBS.vue";
 export default {
   props: ["blocs", "displayName"],
-  components: { BannerFull, BannerStack, BlogPosts },
+  components: { BannerFull, BannerStack, BlogPosts, ContentSBS },
   methods: {
     getBlocName(bloc) {
       if (bloc.acf_fc_layout == "Banner") {
@@ -31,6 +32,7 @@ export default {
     padding: 4rem;
     border: solid 1px black;
     margin-bottom: 8rem;
+    margin-top: 8rem;
   }
 }
 </style>
---
+++ components/blocs/ContentSBS.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+.contentSBS(:class="inverted ? 'inverted' : null")
+  Container.container--small
+    .image(v-if="bloc.image")
+      img(:src="bloc.image.mobile.src")
+    .text
+      h2.h2(v-html="bloc.titre", v-if="bloc.titre")
+      .description(v-html="bloc.description", v-if="bloc.description")
+      Btn.core(
+        v-if="bloc.bouton",
+        :href="bloc.bouton.url",
+        :target="bloc.bouton.target"
+      )
+        span(v-html="bloc.bouton.title")
+</template>
+
+<script>
+export default {
+  props: ["bloc"],
+  computed: {
+    inverted() {
+      return this.bloc.alignement_image == "right";
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.contentSBS {
+  .container {
+    display: flex;
+    justify-content: space-between;
+  }
+  .description {
+    margin-top: 4rem;
+  }
+  .btn {
+    margin-top: 2rem;
+  }
+  .image {
+    width: 44rem;
+    flex-shrink: 0;
+    max-width: 100%;
+    height: auto;
+    img {
+      width: 100%;
+      height: auto;
+      border-radius: 0.5rem;
+    }
+  }
+}
+
+.contentSBS:not(.inverted) {
+  .image {
+    margin-right: 5rem;
+  }
+}
+.contentSBS.inverted {
+  .container {
+    flex-direction: row-reverse;
+  }
+  .image {
+    margin-left: 5rem;
+  }
+}
+</style>

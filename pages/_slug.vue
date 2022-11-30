bf4a0546e9c62e664265891611b0ea36f97928b9
Vincent Dilly <vdilly.prod@gmail.com>
2022-11-30 13:51:55 +0100

    remove unused component declaration

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -4,12 +4,11 @@ main(v-if="page")
 </template>
 <script>
 import pageMixin from "@/mixins/page/page";
-import BannerStack from "@/components/blocs/Banners/BannerStack.vue";
 import TemplatePage from "@/templates/TemplatePage.vue";
 export default {
   layout: "Default",
   mixins: [pageMixin],
-  components: { BannerStack, TemplatePage },
+  components: { TemplatePage },
   async asyncData({ store, params, error }) {
     let page = store.getters["pages/getPageBySlug"](params.slug);
     if (!page) return error({ statusCode: 404, message: "Page introuvable" });
